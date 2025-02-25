ba8843df43aa6cdb85a54ed96da8a5ca9d8b9075
Kingson.Arum <nwajiudo@gmail.com>
2025-02-25 22:42:36 +0100

    generate random number function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -36,13 +36,14 @@ import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/V
 contract Raffle is VRFConsumerBaseV2Plus {
     error Raffle__NotEnoughEthSent();
 
-    uint256 private constant REQUEST_CONFIRMATIONS = 3;
-    uint256 private constant NUM_WORDS = 1;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
     bytes32 private immutable i_keyHash;
     uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
     /** Events */
@@ -54,8 +55,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
         address vrfCoordinator,
         bytes32 gasLane,
         uint256 subscriptionId,
-        uint32 callbackGasLimit,
-        uint32 numWords
+        uint32 callbackGasLimit
     ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_subscriptionId = subscriptionId;
         i_entranceFee = entranceFee;
@@ -78,22 +78,13 @@ contract Raffle is VRFConsumerBaseV2Plus {
             revert();
         }
         //Here we pick a random winner using Chainlink VRF
-        // uint256 requestId = i_vrfCoordinator.requestRandomWords(
-        //     VRFV2PlusClient.RandomWordsRequest(
-        //         i_gasLane,
-        //         i_subscriptionId,
-        //         REQUEST_CONFIRMATIONS,
-        //         i_callbackGasLimit,
-        //         NUM_WORDS
-        //     )
-        // );
 
-        VRFV2PlusClient.RandomWordsRequest request = VRFV2PlusClient
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
             .RandomWordsRequest({
                 keyHash: i_keyHash,
                 subId: i_subscriptionId,
                 requestConfirmations: REQUEST_CONFIRMATIONS,
-                callbackGasLimit: callbackGasLimit,
+                callbackGasLimit: i_callbackGasLimit,
                 numWords: NUM_WORDS,
                 extraArgs: VRFV2PlusClient._argsToBytes(
                     // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
@@ -107,4 +98,9 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal override {}
 }
